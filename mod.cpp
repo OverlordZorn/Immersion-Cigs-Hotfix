f609e52f249b1a273d95329d223cdf4bb0a99400
Mr. Zorn <dennisAzorn@gmail.com>
2024-01-21 16:00:02 +0100

    init

---
+++ mod.cpp
@@ -0,0 +1,24 @@
+name = "Immersion Cigs Hotfix";
+
+author = "Mr. Zorn";
+
+picture         = "mod-hf.paa";                                                                                                                        // Picture displayed from the expansions menu. Optimal size is 2048x1024
+hideName        = "false";                                                                                                                          // Hide the extension name
+hidePicture     = "false";                                                                                                                          // Hide the extension menu
+
+logoSmall       = "mod-hf.paa";                                                                                                    // Display next to the item added by the mod 64x64
+logo            = "mod-hf.paa";                                                                                                   // Logo displayed in the main menu 128x128
+logoOver        = "mod-hf.paa";                                                                                                     // When the mouse is over, in the main menu 128x128
+
+tooltip         = "Immersion Cigs Hotfix";
+action          = "http://discord.chornivoron.net/";                                                                                                // Website URL, that can accessed from the expansions menu 
+actionName      = "Github of Hotfix";                                                                                                                        // label of button/tooltip in extension menu
+
+// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] =
+{
+    0.412,
+    0.0,
+    0.0,
+    1
+};
