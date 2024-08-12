1172da59b1fbd072a467c554e329f6219d6e1b29
Mr. Zorn <dennisAzorn@gmail.com>
2024-08-12 15:57:46 +0200

    updated meta files

--- mod.cpp
+++ mod.cpp
@@ -11,7 +11,7 @@ logo            = "mod-hf.paa";                                                 
 logoOver        = "mod-hf.paa";                                                                                                     // When the mouse is over, in the main menu 128x128
 
 tooltip         = "Immersion Cigs Hotfix";
-action          = "https://github.com/PulsarNeutronStar/Immersion-Cigs-Hotfix";                                                                                                // Website URL, that can accessed from the expansions menu 
+action          = "https://github.com/OverlordZorn/Immersion-Cigs-Hotfix";                                                                                                // Website URL, that can accessed from the expansions menu 
 actionName      = "Github of Hotfix";                                                                                                                        // label of button/tooltip in extension menu
 
 // Color used for DLC stripes and backgrounds (RGBA)
